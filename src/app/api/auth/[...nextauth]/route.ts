9364877839e2351b2886c015682211f4d0c76654
Joydeep Bhattacharjee <2jbhattacharjee@gmail.com>
2025-04-20 13:44:37 +0530

    Add access token to session.

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -11,10 +11,45 @@ const handler = NextAuth({
         params: {
           scope:
             "openid profile email https://www.googleapis.com/auth/youtube https://www.googleapis.com/auth/youtube.force-ssl",
+          prompt: "consent",
+          access_type: "offline",
         },
       },
     }),
   ],
+  pages: {
+    signIn: "/auth/signin",
+    signOut: "/auth/signout",
+    error: "/auth/error",
+  },
+  session: {
+    strategy: "jwt",
+  },
+
+  callbacks: {
+    async redirect({ url, baseUrl }) {
+      return `${baseUrl}/dashboard`;
+    },
+    async jwt({ token, account }) {
+      console.log("JWT Callback - Token:", token);
+      console.log("JWT Callback - Account:", account);
+      if (account) {
+        token.accessToken = account.access_token;
+        token.refreshToken = account.refresh_token;
+        token.expiresAt = account.expires_at;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      console.log("Session Callback - Session:", session);
+      console.log("Session Callback - Token:", token);
+
+      session.accessToken = token.accessToken as string;
+      session.refreshToken = token.refreshToken as string;
+      session.expiresAt = token.expiresAt as number;
+      return session;
+    },
+  },
   adapter,
   secret: process.env.NEXTAUTH_SECRET,
 });
